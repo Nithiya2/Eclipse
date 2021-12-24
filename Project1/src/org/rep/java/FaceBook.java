3605c069fa33a0e93914ac80098efc41e8036c29
Nithiya2 <96539182+Nithiya2@users.noreply.github.com>
2021-12-24 20:09:54 +0530

    Add files via upload

---
+++ Project1/src/org/rep/java/FaceBook.java
@@ -0,0 +1,20 @@
+package org.rep.java;
+
+public class FaceBook {
+	
+	private void emailId() {
+		// TODO Auto-generated method stub
+    System.out.println("My emailid is az@gmail.com");
+	}
+
+	private void passWord() {
+	// TODO Auto-generated method stub
+    System.out.println("My password is abcde");
+}
+
+public static void main(String[] args) {
+	FaceBook f= new FaceBook();
+    f.emailId();
+    f.passWord();
+}
+}
